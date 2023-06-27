c2771d11b775072261bac1b25346807c32c306ea
Guduru Kiran Sai <119746361+kiransais03@users.noreply.github.com>
2023-06-27 18:58:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const getSumBtn = document.createElement("button");
+let pricearr;
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
-const getSum = () => {
+const getSum = (event) => {
 //Add your code here
-  
-};
+  pricearr = document.querySelectorAll('.price');
+	let ans=Array.from(pricearr).reduce(myfunc,0);
+
+    function myfunc(prevval,currelem,index,arr) {
+    	return prevval+parseInt(currelem.innerText);
+    }	
+	
+    let table=document.getElementsByTagName('table')[0];
+  let trow= table.insertRow();
+  let cell=trow.insertCell(0);
+ cell.innerText=`${ans}`;
+}
 
 getSumBtn.addEventListener("click", getSum);
 
